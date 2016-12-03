119af067eb8353813c878419bdc1b905f793f9b5
tatecottom123 <tatecottom123@gmail.com>
2016-12-03 11:22:40 -0500

    FBLA App
    Updated. Working Login and Register

--- LoginFBLA/app/src/main/java/com/example/tate/loginfbla/RegisterActivity.java
+++ LoginFBLA/app/src/main/java/com/example/tate/loginfbla/RegisterActivity.java
@@ -1,69 +1,66 @@
 package com.example.tate.loginfbla;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Button;
-import android.view.View;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText Name,Email,Pass,ConPass;
-    Button reg_button;
-    AlertDialog.Builder builder;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Name = (EditText)findViewById(R.id.reg_name);
-        Email = (EditText)findViewById(R.id.email);
-        Pass = (EditText)findViewById(R.id.password);
-        ConPass = (EditText)findViewById(R.id.reg_con_password);
-        reg_button = (Button)findViewById(R.id.reg_button);
-        reg_button.setOnClickListener(new View.OnClickListener() {
+
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-    if(Name.getText().toString().equals("")||Email.getText().toString().equals("")|| Pass.getText().toString().equals(""))
-    {
-        builder = new AlertDialog.Builder(RegisterActivity.this);
-        builder.setTitle("Something went wrong...");
-        builder.setMessage("Please fill all the fields..");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-               dialog.dismiss();
-            }
+                final String name = etName.getText().toString();
+                final String email = etEmail.getText().toString();
+                final int age = Integer.parseInt(etAge.getText().toString());
+                final String password = etPassword.getText().toString();
 
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-                else if (!(Pass.getText().toString().equals(ConPass.getText().toString())))
-    {
-        builder = new AlertDialog.Builder(RegisterActivity.this);
-        builder.setTitle("Something went wrong...");
-        builder.setMessage("Your passwords are not matching..");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                Pass.setText("");
-                ConPass.setText("");
-            }
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
 
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-                else
-    {
-       BackgroundTask backgroundTask = new BackgroundTask(RegisterActivity.this);
-        backgroundTask.execute("register",Name.getText().toString(),Email.getText().toString(),Pass.getText().toString());
-
-
-    }
+                RegisterRequest registerRequest = new RegisterRequest(name, email, age, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
             }
         });
     }
